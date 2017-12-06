01ee954f95b59cca809258097b0600fda58075fa
Indrajeet Singh Mandloi <indrajeet925@gmail.com>
2017-12-05 22:19:35 -0600

    Created start button/startGame function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
- 
+ //Start button for starting the count/game
+ function startGame(){
+ var number = document.getElementById("given_num").value;
+ //document.getElementById("test").innerHTML = number;
+ var count = 0;
+ var interval = setInterval(function () {
+	count++;
+	document.getElementById("test").innerHTML = count;
+	if (count % 3 == 0){
+		document.getElementById("box1").style.backgroundColor = 'red';
+	}
+	else{
+		document.getElementById("box1").style.backgroundColor = 'white';
+	}
+	if(count % 5 ==0)
+	{
+		document.getElementById("box2").style.backgroundColor = 'red';
+	}
+	else{
+		document.getElementById("box2").style.backgroundColor = 'white';
+	}
+	
+	if (count >= number)
+	{clearInterval(interval);}
+	}, 1000);
+}
