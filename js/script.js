66cb2688adb4f7da0ecbf228dfdf97f37a70c32e
Indrajeet Singh Mandloi <indrajeet925@gmail.com>
2017-12-05 23:20:09 -0600

    Created javascript functions for reset and restart button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+ //Global Variable
+ var interval ;
  //Start button for starting the count/game
  function startGame(){
  var number = document.getElementById("given_num").value;
  //document.getElementById("test").innerHTML = number;
  var count = 0;
- var interval = setInterval(function () {
+ interval = setInterval(function () {
 	count++;
 	document.getElementById("test").innerHTML = count;
 	if (count % 3 == 0){
@@ -23,4 +25,22 @@
 	if (count >= number)
 	{clearInterval(interval);}
 	}, 1000);
+}
+
+//Reset button for reseting the count/game
+function resetGame(){
+	clearInterval(interval);
+	document.getElementById("given_num").value = "";
+	document.getElementById("test").innerHTML = 0;
+	document.getElementById("box1").style.backgroundColor = 'white';
+	document.getElementById("box2").style.backgroundColor = 'white';
+}
+
+////Restart button for restarting the count/game
+function restartGame(){
+	clearInterval(interval);
+	document.getElementById("test").innerHTML = 0;
+	document.getElementById("box1").style.backgroundColor = 'white';
+	document.getElementById("box2").style.backgroundColor = 'white';
+	startGame();
 }
